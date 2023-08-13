b549f64ff05e51e2437e7cd5b68f36c80bd03298
Sho Kaneko <kanekoshoyu@gmail.com>
2023-08-13 14:59:29 +0100

    refactor: pinngpong

--- src/bin/ws_pingpong.rs
+++ src/bin/ws_pingpong.rs
@@ -1,32 +1,7 @@
-use axum::{
-    extract::{
-        ws::{Message, WebSocket, WebSocketUpgrade},
-        TypedHeader,
-    },
-    response::IntoResponse,
-    routing::get,
-    Router,
-};
+use axum::extract::ws::{Message, WebSocket, WebSocketUpgrade};
+use axum::extract::TypedHeader;
+use axum::Router;
 use std::net::SocketAddr;
-
-#[tokio::main]
-async fn main() {
-    if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", "example_websockets=debug,tower_http=debug")
-    }
-    chaiwala::logger::log_init();
-
-    let app = Router::new()
-        .route("/", get(handle_http))
-        //绑定websocket路由
-        .route("/ws", get(handle_websocket));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    log::info!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
 
 async fn handle_http() -> &'static str {
     "Hello, World!"
@@ -35,15 +10,15 @@ async fn handle_http() -> &'static str {
 async fn handle_websocket(
     ws: WebSocketUpgrade,
     user_agent: Option<TypedHeader<headers::UserAgent>>,
-) -> impl IntoResponse {
+) -> impl axum::response::IntoResponse {
     if let Some(TypedHeader(user_agent)) = user_agent {
         log::info!("Connected: {}", user_agent.as_str());
     }
 
-    ws.on_upgrade(handle_websocket_client)
+    ws.on_upgrade(ws_callback)
 }
 
-async fn handle_websocket_client(mut socket: WebSocket) {
+async fn ws_callback(mut socket: WebSocket) {
     loop {
         let res = socket.recv().await;
         if res.is_none() {
@@ -70,4 +45,23 @@ async fn handle_websocket_client(mut socket: WebSocket) {
         }
     }
     log::warn!("Escaping handler");
+}
+
+#[tokio::main]
+async fn main() {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "example_websockets=debug,tower_http=debug")
+    }
+    chaiwala::logger::log_init();
+
+    let app = Router::new()
+        .route("/", axum::routing::get(handle_http))
+        //绑定websocket路由
+        .route("/ws", axum::routing::get(handle_websocket));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    log::info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
