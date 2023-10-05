a3e664392d29d6c7f285392086cffa184abc3ecd
Sho Kaneko <kanekoshoyu@gmail.com>
2023-10-05 15:15:03 +0100

    make config accessible

--- src/bin/test_toml.rs
+++ src/bin/test_toml.rs
@@ -1,6 +1,6 @@
 use chaiwala::config as config;
 
 fn main() {
-    let config = config::get_config("config.toml");
+    let config = config::from_file("config.toml");
     println!("{config:?}");
 }
--- src/config.rs
+++ src/config.rs
@@ -3,30 +3,30 @@ use std::fs;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-    kucoin: KuCoin,
-    discord: Discord,
-    behaviour: Behaviour,
+    pub kucoin: KuCoin,
+    pub discord: Discord,
+    pub behaviour: Behaviour,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct KuCoin {
-    api_key: String,
-    secret_key: String,
-    passphrase: String,
+    pub api_key: String,
+    pub secret_key: String,
+    pub passphrase: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Discord {
-    api_key: String,
+    pub api_key: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Behaviour {
-    monitor_interval_sec: u32,
-    usd_cyclic_arbitrage: u32,
+    pub monitor_interval_sec: u32,
+    pub usd_cyclic_arbitrage: u32,
 }
 
-pub fn get_config(filename: &str) -> Config {
+pub fn from_file(filename: &str) -> Config {
     let toml_str = fs::read_to_string(filename).expect("Failed to read config.toml");
     toml::from_str(&toml_str).unwrap()
 }
